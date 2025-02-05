00bb1b188e36ea9d3425c5e677972955ee46a3ca
mohamed essam <mohamedessam2003147@gmail.com>
2025-02-05 21:07:57 +0200

    create adabtive file and splash view file

---
+++ lib/core/widgets/addabtive_layout.dart
@@ -0,0 +1,27 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+
+import '../utils/size_config.dart';
+
+class AdaptiveLayout extends StatelessWidget {
+  const AdaptiveLayout(
+      {super.key,
+      required this.mobileLayout,
+      required this.tabletLayout,
+      required this.desktopLayout});
+  final WidgetBuilder mobileLayout, tabletLayout, desktopLayout;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constrains) {
+      log("myWidth: ${constrains.maxWidth.toString()}");
+      if (constrains.maxWidth < SizeConfig.tabletsize) {
+        return mobileLayout(context);
+      } else if (constrains.maxWidth < SizeConfig.desktopSize) {
+        return tabletLayout(context);
+      } else {
+        return desktopLayout(context);
+      }
+    });
+  }
+}
