1928cdf047c6c212990f1dbc4307560cd04dbbd1
mohamed essam <mohamedessam2003147@gmail.com>
2025-03-03 13:58:52 +0200

    create auth repo implementation

--- lib/features/auth/data/auth_repos_impl/auth_repos_impl.dart
+++ lib/features/auth/data/auth_repos_impl/auth_repos_impl.dart
@@ -1,1 +1,55 @@
 // create auth repos implementaion
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:movies_app/core/errors/failure.dart';
+import 'package:movies_app/core/errors/firebase_signup_errors.dart';
+import 'package:movies_app/core/services/firebase_auth_service.dart';
+import 'package:movies_app/features/auth/data/models/user_model.dart';
+import 'package:movies_app/features/auth/domain/entity/user_entity.dart';
+import 'package:movies_app/features/auth/domain/repos/auth_repos.dart';
+
+import '../../../../core/errors/firebase_login_errors.dart';
+
+class AuthReposImpl extends AuthRepos {
+  final FirebaseAuthService firebaseAuthService;
+
+  AuthReposImpl({required this.firebaseAuthService});
+
+  @override
+  Future<Either<Failure, UserEntity>> login(
+      String email, String password) async {
+    User? user;
+    try {
+      user = await firebaseAuthService.signIn(email, password);
+      UserEntity userEntity = UserModel.fromfirebase(user: user);
+      return Right(userEntity);
+    } on FirebaseLoginFailure catch (error) {
+      return Left(
+        Failure(error.message),
+      );
+    } catch (error) {
+      return left(
+        Failure("Unknown Error, try later"),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserEntity>> singup(
+      String email, String password) async {
+    User? user;
+    try {
+      user = await firebaseAuthService.signUp(email, password);
+      UserEntity userEntity = UserModel.fromfirebase(user: user);
+      return right(userEntity);
+    } on FirebaseSignupFailure catch (error) {
+      return left(
+        Failure(error.message),
+      );
+    } catch (error) {
+      return left(
+        Failure("Unknown Error, try later"),
+      );
+    }
+  }
+}
