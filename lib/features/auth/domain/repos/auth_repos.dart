95aa9d65591d6c9de8f7a55962fcc1e84d4bce37
mohamed essam <mohamedessam2003147@gmail.com>
2025-03-25 16:31:06 +0200

    create facebook repo implementation

--- lib/features/auth/domain/repos/auth_repos.dart
+++ lib/features/auth/domain/repos/auth_repos.dart
@@ -6,9 +6,17 @@ import 'package:movies_app/features/auth/domain/entity/user_entity.dart';
 import '../../../../core/errors/failure.dart';
 
 abstract class AuthRepos {
+  // create signup repo;
   Future<Either<Failure, UserEntity>> singup(
       {required String email, required String password});
+
+  // create login repo;
   Future<Either<Failure, UserEntity>> login(
       {required String email, required String password});
+
+  // create login with google repo;
   Future<Either<Failure, UserEntity>> signInWithGoogle();
+
+  // create login with facebook repo;
+  Future<Either<Failure, UserEntity>> signInWithFacebook();
 }
