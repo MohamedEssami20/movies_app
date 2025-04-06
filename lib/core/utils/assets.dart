8026687110cf8de92fc7994050f7404f68df37a1
mohamed essam <mohamedessam2003147@gmail.com>
2025-04-06 16:33:18 +0200

    create now palying movies model

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,14 +1,15 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-  
+
   /// Assets for assetsImagesAppleIcon
   /// assets/images/apple_icon.svg
   static const String assetsImagesAppleIcon = "assets/images/apple_icon.svg";
 
   /// Assets for assetsImagesFacebookIcon
   /// assets/images/facebook_icon.svg
-  static const String assetsImagesFacebookIcon = "assets/images/facebook_icon.svg";
+  static const String assetsImagesFacebookIcon =
+      "assets/images/facebook_icon.svg";
 
   /// Assets for assetsImagesFilmImage
   /// assets/images/film_image.png
@@ -16,7 +17,8 @@ class Assets {
 
   /// Assets for assetsImagesFilterListIcon
   /// assets/images/filter_list_icon.svg
-  static const String assetsImagesFilterListIcon = "assets/images/filter_list_icon.svg";
+  static const String assetsImagesFilterListIcon =
+      "assets/images/filter_list_icon.svg";
 
   /// Assets for assetsImagesGoogleIcon
   /// assets/images/google_icon.svg
@@ -32,14 +34,15 @@ class Assets {
 
   /// Assets for assetsImagesOnBoardingBackground2
   /// assets/images/on_boarding_background2.png
-  static const String assetsImagesOnBoardingBackground2 = "assets/images/on_boarding_background2.png";
+  static const String assetsImagesOnBoardingBackground2 =
+      "assets/images/on_boarding_background2.png";
 
   /// Assets for assetsImagesSplahMovies
   /// assets/images/splah_movies.jpg
-  static const String assetsImagesSplahMovies = "assets/images/splah_movies.jpg";
+  static const String assetsImagesSplahMovies =
+      "assets/images/splah_movies.jpg";
 
   /// Assets for assetsImagesVideoIcon
   /// assets/images/video_icon.svg
   static const String assetsImagesVideoIcon = "assets/images/video_icon.svg";
 }
-
