425e9a4ad8698d1ebf0068e70602aa5144fc3403
mohamed essam <mohamedessam2003147@gmail.com>
2025-04-06 17:21:11 +0200

    create get now playing movies in home repos impl

---
+++ lib/features/home/data/home_repos_impl/home_repos_impl.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:movies_app/core/errors/failure.dart';
+import 'package:movies_app/features/home/data/data_source/home_data_source_repos.dart';
+import 'package:movies_app/features/home/domain/entities/now_playing_entity.dart';
+import 'package:movies_app/features/home/domain/home_repos/home_repos.dart';
+
+class HomeReposImpl implements HomeRepos {
+  HomeDataSourceRepos homeDataSourceRepos;
+  HomeReposImpl({required this.homeDataSourceRepos});
+  @override
+  Future<Either<Failure, NowPlayingEntity>> getNowPlayingMovies()async {
+    return await homeDataSourceRepos.getNowPlayingMovies();
+  }
+}
