569c52ea820695e8f43be5e43fe43575af624cfc
mohamed essam <mohamedessam2003147@gmail.com>
2025-04-21 16:59:12 +0200

    create top rating movies cubit

--- lib/features/home/domain/entities/top_rating_movies_entity/top_rating_movies_entity.dart
+++ lib/features/home/domain/entities/top_rating_movies_entity/top_rating_movies_entity.dart
@@ -1,10 +1,11 @@
 
 
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:movies_app/features/home/domain/entities/base_movies_entity.dart';
 part 'top_rating_movies_entity.g.dart';
 
 @HiveType(typeId: 3)
-class TopRatingMoviesEntity extends HiveObject{
+class TopRatingMoviesEntity extends HiveObject implements BaseMoviesEntity{
   @HiveField(0)
   final int movieId;
   @HiveField(1)
@@ -16,4 +17,7 @@ class TopRatingMoviesEntity extends HiveObject{
       {required this.movieId,
       required this.movieTitle,
       required this.moviePoster});
+
+ @override
+  int get baseMoviesId => movieId;
 }
--- lib/features/home/presentation/manager/top_rating_movies/top_rating_movies_cubit.dart
+++ lib/features/home/presentation/manager/top_rating_movies/top_rating_movies_cubit.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:movies_app/features/home/domain/home_repos/home_repos.dart';
 
+import '../../../../../core/func/check_if_new_movies_equal_current_movies.dart';
 import '../../../domain/entities/top_rating_movies_entity/top_rating_movies_entity.dart';
 
 part 'top_rating_movies_state.dart';
@@ -15,5 +16,31 @@ class TopRatingMoviesCubit extends Cubit<TopRatingMoviesState> {
   // create method that get top rating movies;
   Future<void> getTopRatingMovies() async {
     emit(TopRatingMoviesLoading());
+    final result = await homeRepos.getTopRatingMovies();
+    result.fold(
+      (failure) {
+        emit(
+          TopRatingMoviesFailure(errorMessage: failure.message),
+        );
+      },
+      (topRatingMovies) {
+        final bool iscurrentMovieEqualNewMovies =
+            checkIfCurrentMoviesEqualNewMovies(
+                currentTopRatingMovies, topRatingMovies);
+        if (iscurrentMovieEqualNewMovies) {
+          emit(
+            TopRatingMoviesSuccess(
+                topRatingMoviesEntityList: currentTopRatingMovies),
+          );
+        } else {
+          currentTopRatingMovies.clear();
+          currentTopRatingMovies.addAll(topRatingMovies);
+          emit(
+            TopRatingMoviesSuccess(
+                topRatingMoviesEntityList: currentTopRatingMovies),
+          );
+        }
+      },
+    );
   }
 }
