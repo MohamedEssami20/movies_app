0889ecb30fc52cfafc43887d4cf9902b86c22fce
mohamed essam <mohamedessam2003147@gmail.com>
2025-05-24 22:15:49 +0200

    create rest of remaining film category methods at home repos

--- lib/features/home/data/home_repos_impl/home_repos_impl.dart
+++ lib/features/home/data/home_repos_impl/home_repos_impl.dart
@@ -4,8 +4,15 @@ import 'package:movies_app/features/home/data/data_source/home_data_source.dart'
 import 'package:movies_app/features/home/data/data_source/home_local_data_source.dart';
 import 'package:movies_app/features/home/domain/entities/action_movies_entity/action_movies_entity.dart';
 import 'package:movies_app/features/home/domain/entities/adventure_movies_entity/adventure_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/animations_movies_entity/animations_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/comedy_movies_entity/comedy_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/crime_movies_entity/crime_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/drama_movies_entity/drama_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/family_movies_entity/family_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/horror_movies_entity/horror_movies_entity.dart';
 import 'package:movies_app/features/home/domain/entities/now_palying_entity/now_playing_entity.dart';
 import 'package:movies_app/features/home/domain/entities/popular_movies_entity/popular_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/romance_movies_entity/romance_movies_entity.dart';
 import 'package:movies_app/features/home/domain/entities/top_rating_movies_entity/top_rating_movies_entity.dart';
 import 'package:movies_app/features/home/domain/entities/trending_movies_entity.dart/trending_movies_entity.dart';
 import 'package:movies_app/features/home/domain/home_repos/home_repos.dart';
@@ -110,6 +117,97 @@ class HomeReposImpl implements HomeRepos {
         homeDataSourceRepos.getAdventureMovies(pageNumber: pageNumber);
     List<AdventureMoviesEntity> homeLocalSource =
         homeLocalDataSource.getAdventureMovies(pageNumber: pageNumber);
+    return getMoviesDataImpl(
+      homeRemoteDataSource: homeRemoteSource,
+      homeLocalDataSource: homeLocalSource,
+    );
+  }
+  
+  // implementation get animations movies from repos;
+  @override
+  Future<Either<Failure, List<AnimationsMoviesEntity>>> getAnimationsMovies({int pageNumber = 1}) {
+    Future<List<AnimationsMoviesEntity>> homeRemoteSource =
+        homeDataSourceRepos.getAnimationsMovies(pageNumber: pageNumber);
+    List<AnimationsMoviesEntity> homeLocalSource =
+        homeLocalDataSource.getAnimationsMovies(pageNumber: pageNumber);
+    return getMoviesDataImpl(
+      homeRemoteDataSource: homeRemoteSource,
+      homeLocalDataSource: homeLocalSource,
+    );
+  }
+  
+  // implementation get comedy movies from repos;
+  @override
+  Future<Either<Failure, List<ComedyMoviesEntity>>> getComedyMovies({int pageNumber = 1}) {
+    Future<List<ComedyMoviesEntity>> homeRemoteSource =
+        homeDataSourceRepos.getComedyMovies(pageNumber: pageNumber);
+    List<ComedyMoviesEntity> homeLocalSource =
+        homeLocalDataSource.getComedyMovies(pageNumber: pageNumber);
+    return getMoviesDataImpl(
+      homeRemoteDataSource: homeRemoteSource,
+      homeLocalDataSource: homeLocalSource,
+    );
+  }
+  
+  // implementation get crime movies from repos;
+  @override
+  Future<Either<Failure, List<CrimeMoviesEntity>>> getCrimeMovies({int pageNumber = 1}) {
+    Future<List<CrimeMoviesEntity>> homeRemoteSource =
+        homeDataSourceRepos.getCrimeMovies(pageNumber: pageNumber);
+    List<CrimeMoviesEntity> homeLocalSource =
+        homeLocalDataSource.getCrimeMovies(pageNumber: pageNumber);
+    return getMoviesDataImpl(
+      homeRemoteDataSource: homeRemoteSource,
+      homeLocalDataSource: homeLocalSource,
+    );
+  }
+  
+  // implementation get drama movies from repos;
+  @override
+  Future<Either<Failure, List<DramaMoviesEntity>>> getDramaMovies({int pageNumber = 1}) {
+    Future<List<DramaMoviesEntity>> homeRemoteSource =
+        homeDataSourceRepos.getDramaMovies(pageNumber: pageNumber);
+    List<DramaMoviesEntity> homeLocalSource =
+        homeLocalDataSource.getDramaMovies(pageNumber: pageNumber);
+    return getMoviesDataImpl(
+      homeRemoteDataSource: homeRemoteSource,
+      homeLocalDataSource: homeLocalSource,
+    );
+  }
+  
+  // implementation get family movies from repos;
+  @override
+  Future<Either<Failure, List<FamilyMoviesEntity>>> getFamilyMovies({int pageNumber = 1}) {
+    Future<List<FamilyMoviesEntity>> homeRemoteSource =
+        homeDataSourceRepos.getFamilyMovies(pageNumber: pageNumber);
+    List<FamilyMoviesEntity> homeLocalSource =
+        homeLocalDataSource.getFamilyMovies(pageNumber: pageNumber);
+    return getMoviesDataImpl(
+      homeRemoteDataSource: homeRemoteSource,
+      homeLocalDataSource: homeLocalSource,
+    );
+  }
+  
+  // implementation get horror movies from repos;
+  @override
+  Future<Either<Failure, List<HorrorMoviesEntity>>> getHorrorMovies({int pageNumber = 1}) {
+    Future<List<HorrorMoviesEntity>> homeRemoteSource =
+        homeDataSourceRepos.getHorrorMovies(pageNumber: pageNumber);
+    List<HorrorMoviesEntity> homeLocalSource =
+        homeLocalDataSource.getHorrorMovies(pageNumber: pageNumber);
+    return getMoviesDataImpl(
+      homeRemoteDataSource: homeRemoteSource,
+      homeLocalDataSource: homeLocalSource,
+    );
+  }
+  
+  // implementation get romance movies from repos;
+  @override
+  Future<Either<Failure, List<RomanceMoviesEntity>>> getRomanceMovies({int pageNumber = 1}) {
+    Future<List<RomanceMoviesEntity>> homeRemoteSource =
+        homeDataSourceRepos.getRomanceMovies(pageNumber: pageNumber);
+    List<RomanceMoviesEntity> homeLocalSource =
+        homeLocalDataSource.getRomanceMovies(pageNumber: pageNumber);
     return getMoviesDataImpl(
       homeRemoteDataSource: homeRemoteSource,
       homeLocalDataSource: homeLocalSource,
--- lib/features/home/domain/home_repos/home_repos.dart
+++ lib/features/home/domain/home_repos/home_repos.dart
@@ -5,7 +5,14 @@ import 'package:movies_app/features/home/domain/entities/top_rating_movies_entit
 import '../../../../core/errors/failure.dart';
 import '../entities/action_movies_entity/action_movies_entity.dart';
 import '../entities/adventure_movies_entity/adventure_movies_entity.dart';
+import '../entities/animations_movies_entity/animations_movies_entity.dart';
+import '../entities/comedy_movies_entity/comedy_movies_entity.dart';
+import '../entities/crime_movies_entity/crime_movies_entity.dart';
+import '../entities/drama_movies_entity/drama_movies_entity.dart';
+import '../entities/family_movies_entity/family_movies_entity.dart';
+import '../entities/horror_movies_entity/horror_movies_entity.dart';
 import '../entities/now_palying_entity/now_playing_entity.dart';
+import '../entities/romance_movies_entity/romance_movies_entity.dart';
 import '../entities/trending_movies_entity.dart/trending_movies_entity.dart';
 import '../entities/up_coming_movies_entity/up_coming_movies_entity.dart';
 
@@ -35,5 +42,33 @@ abstract class HomeRepos {
 
   // create method that get adventure movies;
   Future<Either<Failure, List<AdventureMoviesEntity>>> getAdventureMovies(
+      {int pageNumber = 1});
+
+  // create method that get comedy movies;
+  Future<Either<Failure, List<ComedyMoviesEntity>>> getComedyMovies(
+      {int pageNumber = 1});
+
+  // create method that get crime movies;
+  Future<Either<Failure, List<CrimeMoviesEntity>>> getCrimeMovies(
+      {int pageNumber = 1});
+
+  // create method that get animation movies;
+  Future<Either<Failure, List<AnimationsMoviesEntity>>> getAnimationsMovies(
+      {int pageNumber = 1});
+
+  // create method that get drama movies;
+  Future<Either<Failure, List<DramaMoviesEntity>>> getDramaMovies(
+      {int pageNumber = 1});
+
+  // create method that get family movies;
+  Future<Either<Failure, List<FamilyMoviesEntity>>> getFamilyMovies(
+      {int pageNumber = 1});
+
+  // create method that get horror movies;
+  Future<Either<Failure, List<HorrorMoviesEntity>>> getHorrorMovies(
+      {int pageNumber = 1});
+
+  // create method that get romance movies;
+  Future<Either<Failure, List<RomanceMoviesEntity>>> getRomanceMovies(
       {int pageNumber = 1});
 }
