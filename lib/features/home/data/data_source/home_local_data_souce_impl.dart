56a661270a96d0baf6361fde8c2b12df4ed2b6ec
mohamed essam <mohamedessam2003147@gmail.com>
2025-05-24 22:09:12 +0200

    create rest of remaining film methods in local data source

--- lib/features/home/data/data_source/home_local_data_souce_impl.dart
+++ lib/features/home/data/data_source/home_local_data_souce_impl.dart
@@ -1,8 +1,15 @@
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:movies_app/features/home/domain/entities/action_movies_entity/action_movies_entity.dart';
 import 'package:movies_app/features/home/domain/entities/adventure_movies_entity/adventure_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/animations_movies_entity/animations_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/comedy_movies_entity/comedy_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/crime_movies_entity/crime_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/drama_movies_entity/drama_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/family_movies_entity/family_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/horror_movies_entity/horror_movies_entity.dart';
 import 'package:movies_app/features/home/domain/entities/now_palying_entity/now_playing_entity.dart';
 import 'package:movies_app/features/home/domain/entities/popular_movies_entity/popular_movies_entity.dart';
+import 'package:movies_app/features/home/domain/entities/romance_movies_entity/romance_movies_entity.dart';
 import 'package:movies_app/features/home/domain/entities/top_rating_movies_entity/top_rating_movies_entity.dart';
 import 'package:movies_app/features/home/domain/entities/trending_movies_entity.dart/trending_movies_entity.dart';
 
@@ -94,6 +101,104 @@ class HomeLocalDataSouceImpl implements HomeLocalDataSource {
   List<AdventureMoviesEntity> getAdventureMovies({int pageNumber = 1}) {
     Box<AdventureMoviesEntity> box =
         Hive.box<AdventureMoviesEntity>(AppConstants.adventureMoviesKey);
+    int startIndex = pageNumber * 0;
+    int endIndex = pageNumber  * 20;
+    bool outOfRange = checkOutOfRange(pageNumber: pageNumber, box: box);
+    if (outOfRange) {
+      return [];
+    }
+    return box.values.toList().sublist(startIndex, endIndex);
+  }
+  
+  // implementation of get animations movies from local storage;
+  @override
+  List<AnimationsMoviesEntity> getAnimationsMovies({int pageNumber = 1}) {
+    Box<AnimationsMoviesEntity> box =
+        Hive.box<AnimationsMoviesEntity>(AppConstants.animationMoviesKey);
+    int startIndex = pageNumber * 0;
+    int endIndex = pageNumber  * 20;
+    bool outOfRange = checkOutOfRange(pageNumber: pageNumber, box: box);
+    if (outOfRange) {
+      return [];
+    }
+    return box.values.toList().sublist(startIndex, endIndex);
+  }
+  
+  // implementation of get comedy movies from local storage;
+  @override
+  List<ComedyMoviesEntity> getComedyMovies({int pageNumber = 1}) {
+    Box<ComedyMoviesEntity> box =
+        Hive.box<ComedyMoviesEntity>(AppConstants.comedyMoviesKey);
+    int startIndex = pageNumber * 0;
+    int endIndex = pageNumber  * 20;
+    bool outOfRange = checkOutOfRange(pageNumber: pageNumber, box: box);
+    if (outOfRange) {
+      return [];
+    }
+    return box.values.toList().sublist(startIndex, endIndex);
+  }
+  
+  // implementation of get crime movies from local storage;
+  @override
+  List<CrimeMoviesEntity> getCrimeMovies({int pageNumber = 1}) {
+    Box<CrimeMoviesEntity> box =
+        Hive.box<CrimeMoviesEntity>(AppConstants.crimeMoviesKey);
+    int startIndex = pageNumber * 0;
+    int endIndex = pageNumber  * 20;
+    bool outOfRange = checkOutOfRange(pageNumber: pageNumber, box: box);
+    if (outOfRange) {
+      return [];
+    }
+    return box.values.toList().sublist(startIndex, endIndex);
+  }
+  
+  // implementation of get drama movies from local storage;
+  @override
+  List<DramaMoviesEntity> getDramaMovies({int pageNumber = 1}) {
+    Box<DramaMoviesEntity> box =
+        Hive.box<DramaMoviesEntity>(AppConstants.dramaMoviesKey);
+    int startIndex = pageNumber * 0;
+    int endIndex = pageNumber  * 20;
+    bool outOfRange = checkOutOfRange(pageNumber: pageNumber, box: box);
+    if (outOfRange) {
+      return [];
+    }
+    return box.values.toList().sublist(startIndex, endIndex);
+  }
+  
+  // implementation of get family movies from local storage;
+  @override
+  List<FamilyMoviesEntity> getFamilyMovies({int pageNumber = 1}) {
+    Box<FamilyMoviesEntity> box =
+        Hive.box<FamilyMoviesEntity>(AppConstants.familyMoviesKey);
+    int startIndex = pageNumber * 0;
+    int endIndex = pageNumber  * 20;
+    bool outOfRange = checkOutOfRange(pageNumber: pageNumber, box: box);
+    if (outOfRange) {
+      return [];
+    }
+    return box.values.toList().sublist(startIndex, endIndex);
+  }
+  
+  // implementation of get horror movies from local storage;
+  @override
+  List<HorrorMoviesEntity> getHorrorMovies({int pageNumber = 1}) {
+    Box<HorrorMoviesEntity> box =
+        Hive.box<HorrorMoviesEntity>(AppConstants.horrorMoviesKey);
+    int startIndex = pageNumber * 0;
+    int endIndex = pageNumber  * 20;
+    bool outOfRange = checkOutOfRange(pageNumber: pageNumber, box: box);
+    if (outOfRange) {
+      return [];
+    }
+    return box.values.toList().sublist(startIndex, endIndex);
+  }
+  
+  // implementation of get romance movies from local storage;
+  @override
+  List<RomanceMoviesEntity> getRomanceMovies({int pageNumber = 1}) {
+    Box<RomanceMoviesEntity> box =
+        Hive.box<RomanceMoviesEntity>(AppConstants.romanceMoviesKey);
     int startIndex = pageNumber * 0;
     int endIndex = pageNumber  * 20;
     bool outOfRange = checkOutOfRange(pageNumber: pageNumber, box: box);
--- lib/features/home/data/data_source/home_local_data_source.dart
+++ lib/features/home/data/data_source/home_local_data_source.dart
@@ -1,8 +1,15 @@
+import 'package:movies_app/features/home/domain/entities/comedy_movies_entity/comedy_movies_entity.dart';
 import 'package:movies_app/features/home/domain/entities/now_palying_entity/now_playing_entity.dart';
+import 'package:movies_app/features/home/domain/entities/romance_movies_entity/romance_movies_entity.dart';
 import 'package:movies_app/features/home/domain/entities/top_rating_movies_entity/top_rating_movies_entity.dart';
 
 import '../../domain/entities/action_movies_entity/action_movies_entity.dart';
 import '../../domain/entities/adventure_movies_entity/adventure_movies_entity.dart';
+import '../../domain/entities/animations_movies_entity/animations_movies_entity.dart';
+import '../../domain/entities/crime_movies_entity/crime_movies_entity.dart';
+import '../../domain/entities/drama_movies_entity/drama_movies_entity.dart';
+import '../../domain/entities/family_movies_entity/family_movies_entity.dart';
+import '../../domain/entities/horror_movies_entity/horror_movies_entity.dart';
 import '../../domain/entities/popular_movies_entity/popular_movies_entity.dart';
 import '../../domain/entities/trending_movies_entity.dart/trending_movies_entity.dart';
 import '../../domain/entities/up_coming_movies_entity/up_coming_movies_entity.dart';
@@ -28,4 +35,25 @@ abstract class HomeLocalDataSource {
 
   // create method that get adventure movies from local storage;
   List<AdventureMoviesEntity> getAdventureMovies({int pageNumber = 1});
+
+  // create method that get comedy movies from local storage;
+  List<ComedyMoviesEntity> getComedyMovies({int pageNumber = 1});
+
+  // create method that get crime movies from local storage;
+  List<CrimeMoviesEntity> getCrimeMovies({int pageNumber = 1});
+
+  // create method that get animation movies from local storage;
+  List<AnimationsMoviesEntity> getAnimationsMovies({int pageNumber = 1});
+
+  // create method that get drama movies from local storage;
+  List<DramaMoviesEntity> getDramaMovies({int pageNumber = 1});
+
+  // create method that get family movies from local storage;
+  List<FamilyMoviesEntity> getFamilyMovies({int pageNumber = 1});
+
+  // create method that get horror movies from local storage;
+  List<HorrorMoviesEntity> getHorrorMovies({int pageNumber = 1});
+
+  // create method that get romance movies from local storage;
+  List<RomanceMoviesEntity> getRomanceMovies({int pageNumber = 1});
 }
