cdeb72c4473f14434ee2ac89e50d79ce3203184a
mohamed essam <mohamedessam2003147@gmail.com>
2025-06-01 15:45:52 +0200

    create search movies method in search remote data source

---
+++ lib/features/search/data/search_data_source/search_remote_data_source_impl.dart
@@ -0,0 +1,21 @@
+import 'package:movies_app/core/services/api_services.dart';
+import 'package:movies_app/features/search/domain/entities/search_movies_entity/search_movies_entity.dart';
+
+import '../../../../core/utils/api_end_points.dart';
+import '../models/search_movies_model.dart';
+import 'search_remote_data_source.dart';
+
+class SearchRemoteDataSourceImpl implements SearchRemoteDataSource {
+  final ApiServices apiServices;
+
+  SearchRemoteDataSourceImpl({required this.apiServices});
+  @override
+  Future<List<SearchMoviesEntity>> searchMovies({required String query}) async{
+    List<SearchMoviesEntity> searchMovies = [];
+    final results= await ApiServices().get("${ApiEndPoints.searchMovies}&query=$query");
+    for (var movies in results['results']) {
+      searchMovies.add(SearchMoviesModel.fromJson(movies));
+    }
+    return searchMovies;
+  }
+}
