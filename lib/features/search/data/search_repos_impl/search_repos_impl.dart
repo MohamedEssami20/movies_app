3a879e6e48a0af42c547b839c2002bcfec5dcf9d
mohamed essam <mohamedessam2003147@gmail.com>
2025-06-01 15:53:33 +0200

    create get search movies method at search repos

---
+++ lib/features/search/data/search_repos_impl/search_repos_impl.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:movies_app/core/errors/api_server_errors.dart';
+
+import 'package:movies_app/core/errors/failure.dart';
+import 'package:movies_app/features/search/data/search_data_source/search_remote_data_source.dart';
+
+import 'package:movies_app/features/search/domain/entities/search_movies_entity/search_movies_entity.dart';
+
+import '../../domain/search_repos/search_repos.dart';
+
+class SearchReposImpl implements SearchRepos {
+  final SearchRemoteDataSource searchRemoteDataSource;
+  SearchReposImpl({required this.searchRemoteDataSource});
+  @override
+  Future<Either<Failure, List<SearchMoviesEntity>>> getSearchMovies(
+      {required String query}) async {
+    try {
+      final result = await searchRemoteDataSource.searchMovies(query: query);
+      return right(result);
+    } on DioException catch (error) {
+      return left(
+        ApiServerErrors.fromDioError(error),
+      );
+    } on Exception {
+      return left (Failure("Opps there was an error, try later."));
+    }
+  }
+}
---
+++ lib/features/search/domain/search_repos/search_repos.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:movies_app/core/errors/failure.dart';
+
+import '../entities/search_movies_entity/search_movies_entity.dart';
+
+abstract class SearchRepos {
+  Future<Either<Failure, List<SearchMoviesEntity>>> getSearchMovies(
+      {required String query});
+}
